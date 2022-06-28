30043545296f5b2f30fb6fcb4d75130095c9b7d3
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-06-28 13:10:04 -0300

    manipulando strings

---
+++ 02-fundamentos/strings.js
@@ -0,0 +1,14 @@
+const escola = 'Joao Paulo I'
+
+console.log(escola.charAt(5))
+console.log(escola.charAt(6))
+console.log(escola.charCodeAt(3))
+console.log(escola.indexOf('P'))
+
+console.log(escola.substring(1))
+console.log(escola.substring(0, 3))
+
+console.log('Escola '.concat(escola).concat('!'))
+console.log(escola.replace('J', 'L'))
+
+console.log('Leonardo,Maria,Pedro'.split(','))
