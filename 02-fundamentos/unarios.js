3db4ee2f2be1927f6a19b872a7918bcda7221021
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-06-30 09:15:18 -0300

    Create unarios.js

---
+++ 02-fundamentos/unarios.js
@@ -0,0 +1,8 @@
+let num1 = 1
+let num2 = 2
+
+num1++
+console.log(num1)
+--num1
+console.log(num1)
+console.log(++num1 === num2--) // o operador unario soma e num1 = 2 compara primeiro e depois diminui
