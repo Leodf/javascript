d053814c202456f6a3745b4cf6e20a38f4938c68
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-07-05 13:18:33 -0300

    Create ex3.js

---
+++ 04-funcoes/lista de exercicios/ex3.js
@@ -0,0 +1,9 @@
+/**
+ *  Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.
+ */
+
+function potencia (base, expoente) {
+    return base ** expoente
+}
+
+console.log(potencia(2, 4))
