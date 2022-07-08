23023a9f9dd0b2be37d3e5ab5a2f0989e3db74f6
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-07-08 13:02:28 -0300

    Update ex20.js

--- 04-funcoes/lista de exercicios/ex20.js
+++ 04-funcoes/lista de exercicios/ex20.js
@@ -1,3 +1,83 @@
 /**
  * Crie um programa para informar quais e quantas notas são necessárias para entregar o mínimo de cédulas para um determinado valor informado pelo usuário considerando notas de R$ 100, R$ 50, R$ 10 e R$ 5 e R$ 1. Seu programa deve mostrar apenas as notas utilizadas. Por exemplo, ao solicitar R$18, o programa deve informar apenas a seguinte informação (note que não foram exibidas informações sobre as demais cédulas): 1 nota(s) de R$ 10. 1 nota(s) de R$ 5. 3 nota(s) de R$ 1.
  */
+
+
+function calcularValorNota(valorSaque) {
+    if (valorSaque >= 100) {
+        return 100
+    } else if (valorSaque >= 50) {
+        return 50
+    } else if (valorSaque >= 10) {
+        return 10
+    } else if (valorSaque >= 5) {
+        return 5
+    } else if (valorSaque >= 1) {
+        return 1
+    }
+}
+
+function elaborarResultado(contador100, contador50, contador10, contador5, contador1) {
+    let resultado = ''
+
+    if (contador100 > 0) {
+        resultado += `${contador100} nota(s) de R$ 100,00 | `
+    }
+    if (contador50 > 0) {
+        resultado += `${contador50} nota(s) de R$ 50,00 | `
+    }
+    if (contador10 > 0) {
+        resultado += `${contador10} nota(s) de R$ 10,00 | `
+    }
+    if (contador5 > 0) {
+        resultado += `${contador5} nota(s) de R$ 5,00 | `
+    }
+    if (contador1 > 0) {
+        resultado += `${contador1} nota(s) de R$ 1,00 | `
+    }
+
+    return resultado
+}
+
+function sacarDinheiro(valorSaque) {
+    let contador100 = 0
+    let contador50 = 0
+    let contador10 = 0
+    let contador5 = 0
+    let contador1 = 0
+    let valorNota = calcularValorNota(valorSaque)
+
+    while (valorSaque >= valorNota){
+        switch (valorNota) {
+            case 100:
+                valorSaque -= 100
+                contador100++
+                break
+            case 50:
+                valorSaque -= 50
+                contador50++
+                break
+            case 10:
+                valorSaque -= 10
+                contador10++
+                break
+            case 5:
+                valorSaque -= 5
+                contador5++
+                break
+            case 1:
+                valorSaque -= 1
+                contador1++
+                break
+        }
+
+        valorNota = calcularValorNota(valorSaque)
+    }
+
+    return elaborarResultado(contador100, contador50, contador10, contador5, contador1)
+}
+
+console.log(sacarDinheiro(177))
+//for(i = 0; i <= 100; i++) {
+   // console.log(notasNecessarias(i))
+//}
