256769fef67e2971c67122429679b66633be16ea
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-07-11 10:15:03 -0300

    Update ex24.js

--- 04-funcoes/lista de exercicios/ex24.js
+++ 04-funcoes/lista de exercicios/ex24.js
@@ -2,8 +2,12 @@
  * Crie um programa que imprima 11 vezes a frase " Hello World!" utilizando uma estrutura de repetição while.
  */
 
-let i = 1
-while (i <= 11) {
+function helloWorld(){
+    let i = 1
+    while (i <= 11) {
     console.log(i, 'Hello World!!')
     i++
+    }
 }
+
+helloWorld()
