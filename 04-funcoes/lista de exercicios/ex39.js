2a257a53b9394d995351f850b1a3eb4e756a1e76
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-07-14 18:11:02 -0300

    Create ex39.js

---
+++ 04-funcoes/lista de exercicios/ex39.js
@@ -0,0 +1,12 @@
+/**
+ * Crie uma função que receba dois vetores de igual tamanho e troque seus elementos de modo que o primeiro elemento do vetorA passe a ser o primeiro elemento do vetorB e vice versa e assim sucessivamente. Faça a troca sem utilizar uma variável auxiliar.
+ */
+
+let vetorA = [1, 2, 3]
+let vetorB = [4, 5, 6]
+
+function trocaPosicaoVetor(vetor1, vetor2) {
+
+}
+
+console.log(vetorA.shift(4))
