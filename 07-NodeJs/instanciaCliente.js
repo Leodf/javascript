4c0354a5ac25158d02f45e03bad64b44de4a5b00
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-07-18 11:14:10 -0300

    Update instanciaCliente.js

--- 07-NodeJs/instanciaCliente.js
+++ 07-NodeJs/instanciaCliente.js
@@ -1,9 +1,13 @@
 const contadorA = require('./instaciaUnica')
 const contadorB = require('./instaciaUnica')
 
-const contadorC = require('./instaciaNova')()
-const contadorD = require('./instaciaNova')()
+const contadorC = require('./instanciaNova')()
+const contadorD = require('./instanciaNova')()
 
 contadorA.inc()
 contadorA.inc()
-console.log(contadorB.valor)
+console.log(contadorA.valor, contadorB.valor)
+
+contadorC.inc()
+contadorC.inc()
+console.log(contadorC.valor, contadorD.valor)
