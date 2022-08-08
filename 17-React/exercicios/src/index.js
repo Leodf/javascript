528902d2c83a716b25eac439d43cb591f6a84041
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-08-08 12:17:18 -0300

    usando o destructuring react

--- 17-React/exercicios/src/index.js
+++ 17-React/exercicios/src/index.js
@@ -2,10 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 // import Primeiro from './components/Primeiro'
-import BomDia from './components/BomDia'
+// import BomDia from './components/BomDia'
+import { BoaTarde, BoaNoite } from './components/Multiplos'
 
 // const elemento = <h1>React 2</h1>
 // ReactDOM.render(elemento, document.getElementById('root'))
 
 // ReactDOM.render(<Primeiro />, document.getElementById('root'))
-ReactDOM.render(<BomDia nome='Guilherme' idade={25} />, document.getElementById('root'))
+// ReactDOM.render(<BomDia nome='Guilherme' idade={25} />, document.getElementById('root'))
+ReactDOM.render(
+    <div>
+        <BoaTarde nome='Ana' />
+        <BoaNoite nome='Bia' />
+    </div>
+, document.getElementById('root'))
