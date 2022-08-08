8af6fb36523e6dba609dcc9a0a4f9a788d5bda1a
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-08-08 12:11:38 -0300

    utilizando o props

--- 17-React/exercicios/src/index.js
+++ 17-React/exercicios/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Primeiro from './components/Primeiro'
+// import Primeiro from './components/Primeiro'
+import BomDia from './components/BomDia'
 
-const elemento = <h1>React 2</h1>
-ReactDOM.render(elemento, document.getElementById('root'))
+// const elemento = <h1>React 2</h1>
+// ReactDOM.render(elemento, document.getElementById('root'))
 
-ReactDOM.render(<Primeiro />, document.getElementById('root'))
+// ReactDOM.render(<Primeiro />, document.getElementById('root'))
+ReactDOM.render(<BomDia nome='Guilherme' idade={25} />, document.getElementById('root'))
