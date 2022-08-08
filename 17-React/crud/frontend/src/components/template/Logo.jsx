2978126367a80abdbdc79e95b8cad176cf14fa2f
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-08-08 16:45:38 -0300

    Update Logo.jsx

--- 17-React/crud/frontend/src/components/template/Logo.jsx
+++ 17-React/crud/frontend/src/components/template/Logo.jsx
@@ -1,9 +1,12 @@
 import './Logo.css'
+import Logo from '../../assets/imgs/logo.png'
 import React from 'react'
 
 const logo = props =>
     <aside className='logo'>
-        logo
+        <a href="/" className="logo">
+            <img src={Logo} alt="logo" />
+        </a>
     </aside>
 
 export default logo
