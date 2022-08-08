679f6a23f2a907a24c14c7592737184425181fc3
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-08-08 12:34:54 -0300

    trabalhando com set State

--- 17-React/exercicios/src/index.js
+++ 17-React/exercicios/src/index.js
@@ -3,16 +3,23 @@ import ReactDOM from 'react-dom'
 
 // import Primeiro from './components/Primeiro'
 // import BomDia from './components/BomDia'
-import { BoaTarde, BoaNoite } from './components/Multiplos'
+// import { BoaTarde, BoaNoite } from './components/Multiplos'
+import Saudacao from './components/Saudacao'
 
 // const elemento = <h1>React 2</h1>
 // ReactDOM.render(elemento, document.getElementById('root'))
 
 // ReactDOM.render(<Primeiro />, document.getElementById('root'))
 // ReactDOM.render(<BomDia nome='Guilherme' idade={25} />, document.getElementById('root'))
+// ReactDOM.render(
+//     <div>
+//         <BoaTarde nome='Ana' />
+//         <BoaNoite nome='Bia' />
+//     </div>
+// , document.getElementById('root'))
+
 ReactDOM.render(
     <div>
-        <BoaTarde nome='Ana' />
-        <BoaNoite nome='Bia' />
+        <Saudacao tipo='Bom dia' nome='João' />
     </div>
 , document.getElementById('root'))
