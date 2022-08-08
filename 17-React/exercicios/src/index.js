f70edd7feb64885635825200a09df3be2b21e83d
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-08-08 12:02:10 -0300

    criando primeiro componente

--- 17-React/exercicios/src/index.js
+++ 17-React/exercicios/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import Primeiro from './components/Primeiro'
+
 const elemento = <h1>React 2</h1>
 ReactDOM.render(elemento, document.getElementById('root'))
+
+ReactDOM.render(<Primeiro />, document.getElementById('root'))
