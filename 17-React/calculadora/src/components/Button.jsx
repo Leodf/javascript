d8b47446ae967d5597504ad2cebec5610cc9af5e
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-08-08 13:54:49 -0300

    recebendo os eventos e estilizando a calculadora

--- 17-React/calculadora/src/components/Button.jsx
+++ 17-React/calculadora/src/components/Button.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import './Button.css'
 
 const botao = props =>
-    <button className="button">{props.label}</button>
+    <button className={`
+        button
+        ${props.operation ? 'operation' : ''}
+        ${props.double ? 'double' : ''}
+        ${props.triple ? 'triple' : ''}
+
+    `} onClick={e => props.click && props.click(props.label)}>{props.label}</button>
 
 export default botao
