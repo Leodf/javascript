b329851188d16f45c91f494965792de8d3a73e2f
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-08-09 11:28:17 -0300

    terminando o layout do menu

--- 17-React/crud/frontend/src/components/template/Nav.jsx
+++ 17-React/crud/frontend/src/components/template/Nav.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 
 const nav = props =>
     <aside className='menu-area'>
-        Navegação
+        <nav className='menu'>
+            {/* Refatorar para colocar os componentes isolados */}
+            <a href="#/">
+                <i className="fa fa-home"></i> Início
+            </a>
+            <a href="#/users">
+                <i className="fa fa-users"></i> Usuários
+            </a>
+        </nav>
     </aside>
 
 export default nav
