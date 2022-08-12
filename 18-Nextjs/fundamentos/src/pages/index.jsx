5607fc0060a7da9e2afa12509028b859f2438220
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-08-12 13:29:27 -0300

    renderizando conteudo statico

--- 18-Nextjs/fundamentos/src/pages/index.jsx
+++ 18-Nextjs/fundamentos/src/pages/index.jsx
@@ -17,6 +17,7 @@ export default function Inicio() {
             <Navegador texto='Navegação Dinamica' destino='/cliente/sp-2/123' cor='blue'/>
             <Navegador texto='Componente com Estado' destino='/estado' cor='pink'/>
             <Navegador texto='Integração com API #01' destino='/integracao_1' cor='cyan'/>
+            <Navegador texto='Conteudo Estático' destino='/estatico' cor='#fa054a'/>
         </div> 
     )
 }
