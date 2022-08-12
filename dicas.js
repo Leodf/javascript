468da50f40ef7fd1b6064ce0218853465bfa11c1
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-08-12 12:51:37 -0300

    diminuindo a quantidade de ifs

---
+++ dicas.js
@@ -0,0 +1,21 @@
+// Reduzindo os Ifs por conjunto chave e valor 
+
+// function diaDaSemana (valor) {
+    
+//     const values = {
+//         'Domingo': 0,
+//         'Segunda': 1,
+//         'Terça': 2,
+//         'Quarta': 3,
+//         'Quinta': 4,
+//         'Sexta': 5,
+//         'Sábado': 6,
+//     }
+
+// }
+
+// const day = 'Segunda'
+// const value = values[day] ?? -1
+
+// console.log(value)
+
