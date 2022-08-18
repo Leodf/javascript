84d32717d2c6f95762de9c69e55c9001beed5064
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-08-18 16:58:21 -0300

    colocando botão e display

---
+++ 19-Vue/calculadora/src/main/CalculatorVue.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="calculator">
+    <DisplayVue />
+    <ButtonVue />
+  </div>
+</template>
+
+<script>
+import DisplayVue from '../components/DisplayVue.vue';
+import ButtonVue from '../components/ButtonVue.vue';
+
+export default {
+    components: { ButtonVue, DisplayVue }
+}
+</script>
+
+<style>
+.calculator {
+    height: 320px;
+    width: 235px;
+    border-radius: 5px;
+    overflow: hidden;
+
+    display: grid;
+    grid-template-columns: repeat(4, 25%);
+    grid-template-rows: 1fr 48px 48px 48px 48px 48px;
+}
+</style>
