7f5b3b9b66f1a0c3d0e839a6b9402261434310c9
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-08-18 17:25:10 -0300

    adicionando as funcionalidades dos botoes

--- 19-Vue/calculadora/src/main/CalculatorVue.vue
+++ 19-Vue/calculadora/src/main/CalculatorVue.vue
@@ -1,7 +1,23 @@
 <template>
   <div class="calculator">
-    <DisplayVue />
-    <ButtonVue />
+    <DisplayVue value="1000"/>
+    <ButtonVue label="AC" triple @onClick="clearMemory"/>
+    <ButtonVue label="/" operation @onClick="setOperation"/>
+    <ButtonVue label="7" @onClick="addDigit"/>
+    <ButtonVue label="8" @onClick="addDigit"/>
+    <ButtonVue label="9" @onClick="addDigit"/>
+    <ButtonVue label="*" operation @onClick="setOperation"/>
+    <ButtonVue label="4" @onClick="addDigit"/>
+    <ButtonVue label="5" @onClick="addDigit"/>
+    <ButtonVue label="6" @onClick="addDigit"/>
+    <ButtonVue label="-" operation @onClick="setOperation"/>
+    <ButtonVue label="1" @onClick="addDigit"/>
+    <ButtonVue label="2" @onClick="addDigit"/>
+    <ButtonVue label="3" @onClick="addDigit"/>
+    <ButtonVue label="+" operation @onClick="setOperation"/>
+    <ButtonVue label="0" double @onClick="addDigit"/>
+    <ButtonVue label="." @onClick="addDigit"/>
+    <ButtonVue label="=" operation @onClick="setOperation"/>
   </div>
 </template>
 
@@ -10,7 +26,18 @@ import DisplayVue from '../components/DisplayVue.vue';
 import ButtonVue from '../components/ButtonVue.vue';
 
 export default {
-    components: { ButtonVue, DisplayVue }
+    components: { ButtonVue, DisplayVue },
+    methods: {
+      clearMemory() {
+        console.log('Limpar Memória!')
+      },
+      setOperation(operation) {
+        console.log('Operação ' + operation)
+      },
+      addDigit(n) {
+        console.log('Digito ' + n)
+      }
+    }
 }
 </script>
 
