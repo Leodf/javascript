28f3655025649c380b70a8d6fda89eab9d1847ba
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-09-20 10:42:30 -0300

    criando consulta personalizada do DB

--- 22-ProjetoFinal/Backend/src/api/article.js
+++ 22-ProjetoFinal/Backend/src/api/article.js
@@ -1,3 +1,5 @@
+const queries = require('./queries')
+
 module.exports = app => {
     const { existsOrError } = app.src.api.validation
 
@@ -33,7 +35,12 @@ module.exports = app => {
         try {
             const rowsDeleted = await app.db('articles')
                 .where({ id: req.params.id }).del()
-            existsOrError(rowsDeleted, 'Artigo não foi encontrado.')
+
+            try {
+                existsOrError(rowsDeleted, 'Artigo não foi encontrado.')
+            } catch(msg) {
+                return res.status(400).send(msg)
+            }
 
             res.status(204).send()
         } catch(msg) {
@@ -66,5 +73,21 @@ module.exports = app => {
             .catch(err => res.status(500).send(err))
     }
 
-    return { save, remove, get, getById }
+    const getByCategory = async (req, res) => {
+        const categoryId = req.params.id
+        const page = req.query.page || 1
+        const categories = await app.db.raw(queries.categoryWithChildren, categoryId)
+        const ids = categories.rows.map(c => c.id)
+
+        app.db({a: 'articles', u: 'users'})
+            .select('a.id', 'a.name', 'a.description', 'a.imageUrl', { author: 'u.name'})
+            .limit(limit).offset(page * limit - limit)
+            .whereRaw('?? = ??', ['u.id', 'a.userId'])
+            .whereIn('categoryId', ids)
+            .orderBy('a.id', 'desc')
+            .then(articles => res.json(articles))
+            .catch(err => res.status(500).send(err))
+    }
+
+    return { save, remove, get, getById, getByCategory }
 }
