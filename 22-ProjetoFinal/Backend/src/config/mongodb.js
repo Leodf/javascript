ed28e83de341a6a1639d777ae0d9f6743ae40946
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-09-21 17:35:34 -0300

    protegendo os dados de autenticação

--- 22-ProjetoFinal/Backend/src/config/mongodb.js
+++ 22-ProjetoFinal/Backend/src/config/mongodb.js
@@ -1,6 +1,8 @@
 
+const { mongodb } = require('../../.env')
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://root:admin@localhost:27017/?authMechanism=DEFAULT', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(`mongodb://${mongodb.username}:${mongodb.password}@${mongodb.host}:${mongodb.port}/?authMechanism=DEFAULT`, { useNewUrlParser: true, useUnifiedTopology: true })
     .catch(e => {
         const msg = 'Erro! Não foi possível conectar com o MongoDB!'
         console.log('\x1b[41m%s\x1b[37m', msg, '\x1b[0m')
