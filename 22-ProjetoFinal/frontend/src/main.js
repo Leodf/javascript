d5f9cc96448f764c31b005118f14ff44461b2906
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-09-29 18:29:17 -0300

    Update main.js

--- 22-ProjetoFinal/frontend/src/main.js
+++ 22-ProjetoFinal/frontend/src/main.js
@@ -7,6 +7,8 @@ import App from './App.vue'
 import store from './config/store'
 import router from './config/router'
 
+// TEMPORARIO
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Ikxlb25hcmRvIGRlIEZhdmVyaSIsImVtYWlsIjoiZmF2ZXJfaUBob3RtYWlsLmNvbSIsImFkbWluIjoidHJ1ZSIsImlhdCI6MTY2NDQ4NTY1NSwiZXhwIjoxNjY0NzQ0ODU1fQ.qhPoJ5-nao9svBQShe3MgvYXTW5tCsmueFHgGy0Jco4'
 
 const app = createApp(App)
 app.use(store)
