3e5c724d1f4754130b20c758b5281d828364f85f
Leonardo de Faveri <83522757+Leodf@users.noreply.github.com>
2022-09-30 14:20:06 -0300

    correção da tabs

--- 22-ProjetoFinal/frontend/src/components/admin/AdminPage.vue
+++ 22-ProjetoFinal/frontend/src/components/admin/AdminPage.vue
@@ -2,13 +2,17 @@
     <div class="admin-pages">
         <PageTitle icon="fa fa-cogs" main="Administração do Sistema" sub="Cadastros & Cia" />
         <div class="admin-pages-tabs">
-            <b-card no-body>
-                <b-tabs card>
-                    <b-tab title="Artigos" active>
-                        <h1>Artigo</h1>
-                    </b-tab>
-                </b-tabs>
-            </b-card>
+            <tabsVue :options="{ useUrlFragment: false }">
+                <tabVue name="Artigos">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, hic incidunt voluptas, tempora perspiciatis ducimus iusto doloremque magni vitae, nesciunt sequi consequuntur. Ut repudiandae atque minima illo maiores reprehenderit magnam?
+                </tabVue>
+                <tabVue name="Second tab">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quae nostrum harum voluptatem iure architecto cum a. Iste et consequatur, fuga nostrum deserunt amet qui inventore repudiandae neque magnam ipsum ex, maxime quia illum minus labore? Quae reiciendis id, aspernatur beatae neque porro aliquid a?
+                </tabVue>
+                <tabVue name="Third tab">
+                    Third tab content
+                </tabVue>
+            </tabsVue>
         </div>
     </div>
 </template>
@@ -18,10 +22,60 @@ import PageTitle from '../template/PageTitle.vue'
 
 export default {
     name: 'AdminPage',
-    components: { PageTitle }
+    components: { PageTitle },
+
 }
 </script>
 
 <style>
+
+    /* .tabs-component {
+        border: solid 1px #ddd;
+    } */
+
+    .tabs-component-tabs {
+        padding: 0;
+        align-items: stretch;
+        display: flex;
+        justify-content: flex-start;
+        margin-bottom: -1px;
+        border-radius: 6px;
+        list-style: none;
+    }
+
+    .tabs-component-tab {
+        margin-right: 0.5rem;
+        background-color: #fff;
+        border: solid 1px #ddd;
+        color: #777;
+        border-radius: 3px 3px 0 0;
+        margin-right: 0.5em;
+        transform: translateY(2px);
+        transition: transform .3s ease;
+    }
+
+    .tabs-component-tab.is-active {
+        border-bottom: solid 1px #fff;
+        z-index: 2;
+        transform: translateY(0);
+        color: red;
+    }
+
+    .tabs-component-tab-a {
+        align-items: center;
+        color: inherit;
+        display: flex;
+        padding: 0.75em 1em;
+        text-decoration: none;
+    }
+
+    .tabs-component-panels {
+        background-color: #fff;
+        border: solid 1px #ddd;
+        border-radius: 3px 3px 0 0;
+        padding: 10px;
+        transform: translateZ(2px);
+        transition: transform .3s ease;
+    }
 
 </style>
--- 22-ProjetoFinal/frontend/src/main.js
+++ 22-ProjetoFinal/frontend/src/main.js
@@ -4,6 +4,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import store from './config/store'
 import router from './config/router'
+import {Tabs, Tab} from 'vue3-tabs-component'
 
 // TEMPORARIO
 require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwibmFtZSI6Ikxlb25hcmRvIGRlIEZhdmVyaSIsImVtYWlsIjoiZmF2ZXJfaUBob3RtYWlsLmNvbSIsImFkbWluIjoidHJ1ZSIsImlhdCI6MTY2NDQ4NTY1NSwiZXhwIjoxNjY0NzQ0ODU1fQ.qhPoJ5-nao9svBQShe3MgvYXTW5tCsmueFHgGy0Jco4'
@@ -11,4 +12,6 @@ require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJK
 const app = createApp(App)
 app.use(store)
 app.use(router)
+app.component('tabsVue', Tabs)
+app.component('tabVue', Tab)
 app.mount('#app')
